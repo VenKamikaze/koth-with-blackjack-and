97ddf72114b16f644fbbe559cffc91365827d905
VenKamikaze <mick.saunders+github@gmail.com>
2025-07-25 23:01:44 +0930

    Update mod_info with latest changelog info.

--- mods/King of the Hill - TSR/mod_info.lua
+++ mods/King of the Hill - TSR/mod_info.lua
@@ -2,7 +2,7 @@ name = "King of the Hill - TSR"
 uid = "tsr-koth-23"
 version = 23
 copyright = ""
-description = "Players receive points for controlling the hill. The first player to reach the point threshold wins!\n\nCredits to AcidVash for the voice overs and JIP for the original KoTH this was forked from!"
+description = "Players receive points for controlling the hill. Tech upgrades unlock after enough points are acquired on the hill. The first player to reach the point threshold wins!\n\nForked from the original King of the Hill by Jip.\nCredits to AcidVash for the voice overs and Jip for the original!\n\nVersion 23\n\nChanges from previous KoTH:\n\nBugfixes:\n* Hill option 'center of spawned players' now works\n* Hill active timer now works\n* Winning score lobby option is now respected\n\nFeatures:\n* Extra hill active timer options\n* Extra total score options\n* Extra 'no penalty' mass option\n* Extra tech unlock options\n* New toggle for commander controlling/contesting a hill\n* Extra tech unlock options (incl. tech 2 start)\n* New hill center spawn option (including AI in spawned players)"
 author = "TSR group, Originally by Jip"
 url = "https://github.com/VenKamikaze/koth-with-blackjack-and"
 
